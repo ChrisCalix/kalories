97c31f34633c84516e6792f09315991eb3601af0
christian.sonic <yoh.chrisitan@gmail.com>
2023-03-08 19:38:46 -0400

    Initial Commit

---
+++ kalories/kaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  kaloriesApp.swift
+//  kalories
+//
+//  Created by Sonic on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct kaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
